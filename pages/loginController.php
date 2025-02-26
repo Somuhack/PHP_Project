3be14324c15353a0344109cf1678b6e1f55f95d9
Arya Sundar Das <140619090+Aryasdas@users.noreply.github.com>
2025-02-26 11:41:13 +0530

    Update loginController.php where we can identify user is admin or not

--- pages/loginController.php
+++ pages/loginController.php
@@ -8,12 +8,16 @@
       $isExist=mysqli_query($conn,$sql);
       if($isExist){
         $row=mysqli_fetch_assoc($isExist);
-       if( $row['pass']==$password){
-        header("location:./ShowAdmissions.php");
-       }else{
-        header("location:../index.php");
-       }
-
+       if($row['pass'] == $password){
+      if($row['isadmin'] == 1){ // Check if the user is an admin
+        header("location:./admin-dashboard.php"); // Redirect to admin dashboard
+      } else {
+        header("location:./ShowAdmissions.php"); // Redirect to user dashboard
       }
-      }
+    } else {
+      header("location:../index.php");
+    }
+  }
+}
 ?>
+
